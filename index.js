d127b6a21d7e843ced531b74a564028792137229
omidvari80 <mlkmhby6@gmail.com>
2022-02-22 02:07:57 +0330

    add : add modal microphone

--- index.js
+++ index.js
@@ -22,3 +22,34 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+  
+/* Get the modal microphone*/
+
+  
+  var microphone = document.getElementById("microphone-Modal");
+  
+  var myBtn = document.getElementById("bot");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName( "multiplied")[0];
+  
+  // When the user clicks the button, open the modal 
+  myBtn.onclick = function() {
+    microphone.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+   microphone.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == microphone) {
+      microphone.style.display = "none";
+    }
+  }
+  
+  
+
